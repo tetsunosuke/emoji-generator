6a82faf46d78a7aedc840ff64ad601337ba673dc
ITO Tetsunosuke <tetsunosuke.ito@photocreate.co.jp>
2020-09-21 11:59:08 +0900

    一文字用（フォントサイズ調整など）

--- emoji_image.rb
+++ emoji_image.rb
@@ -1,6 +1,8 @@
 require 'rmagick'
 require 'pry'
 require 'securerandom'
+require 'nkf'
+
 
 include Magick
 
@@ -14,32 +16,23 @@ class EmojiImage
 
     d = Magick::Draw.new
 
-    padding = 4
-    fontsize =  (64 - padding * 2) / 2
+    padding = 1
+    fontsize = 64 - padding * 5
     color = ['#F44336', '#9C27B0', '#3F51B5', '#03A9F4', '#009688', '#8BC34A', '#FFEB3B', '#FF9800', '#795548'].sample
 
-    text.split('').to_a.each_slice((text.size / 2.0).round).with_index do |row, index|
-      p index
-      row_text = row.join
-
-      d.annotate(f, 0, 0, padding, padding + index * fontsize, row_text) do
-        self.pointsize = fontsize
-        self.fill = color
-        self.font = 'NotoSansCJKjp-Medium.otf'
-        self.gravity = Magick::NorthGravity
-      end
+    d.annotate(f, 0, 0, padding, -12, text) do
+      self.pointsize = fontsize
+      self.fill = color
+      self.font = 'MPLUSRounded1c-Bold.ttf'
+      self.gravity = Magick::NorthGravity
     end
 
-    @path = "public/dest-#{SecureRandom.base64}.png"
+    @path = "#{text}.png"
     f.write(@path)
   end
 end
 
-text = ARGV[0]
-unless text
-  puts 'Require args: image path'
-  exit
-end
+"あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわをんがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽぁぃぅぇぉゃゅょっ".chars.each { |chr|
+  image = EmojiImage.new(chr)
+}
 
-image = EmojiImage.new(text)
-`open #{image.path}`
